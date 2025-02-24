173ae56004a07e6c5dbb7ea2f451529a2e73db7e
ValentinoBellini <valentinobellini.dev@gmail.com>
2025-02-24 15:24:50 +0100

    crete layout, pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import './App.css'
+// import './App.css'
+
+// importo elementi react-router-dom
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
 
 
   return (
     <>
+      <h1>hello world</h1>
 
     </>
   )
